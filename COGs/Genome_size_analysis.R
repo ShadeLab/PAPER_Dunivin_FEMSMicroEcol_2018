3ffca33cf6b6985c12ae27031aa97a2dcbb14450
dunivint <dunivint@Taylors-MacBook-Pro-4.local>
2017-04-21 11:40:53 -0400

    COG plots

--- COGs/Genome_size_analysis.R
+++ COGs/Genome_size_analysis.R
@@ -3,23 +3,29 @@ library(reshape2)
 library(ggplot2)
 library(psych)
 
+#print working directory for future references
+wd=print(getwd())
+
 ##View trend of average genome size 
 #read in microbe census data
-data=read.delim("microbe_census.txt")
+data=read.delim(file = paste(wd, "/data/microbe_census.txt", sep=""))
 
 #read in site information
-meta=read.delim("Cen_temp_As.txt", header=FALSE)
+meta=read.delim(file = paste(wd, "/data/Cen_temp_As.txt", sep=""), header = FALSE)
 colnames(meta)=c("Site", "Temperature", "As", "Classification")
 
 #join datasets to annotate microbe census data
 data=inner_join(data, meta, by="Site")
 
 #plot data
-ggplot(data, aes(x=Temperature, y=AGS/1000000)) +
-  geom_point(aes(shape=Classification), size=3) +
+(AGS <- ggplot(data, aes(x=Temperature, y=AGS/1000000)) +
   geom_smooth(method="lm") + 
+  geom_point(aes(shape=Classification), size=3) +
   ylab("Average genome size (Mbp)") +
-  theme_classic(base_size = 12)
+  theme_classic(base_size = 12))
+
+#save plot
+ggsave(AGS, filename = paste(wd, "/figures/AGS.temp.png", sep=""))
 
 ##calculate correlation
 #subset data
