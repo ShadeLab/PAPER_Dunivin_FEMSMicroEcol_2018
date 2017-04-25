a12b961e26fd1280933709617480065bdc836bd8
dunivint <dunivint@Taylors-MacBook-Pro-4.local>
2017-04-25 17:02:05 -0400

    cog

--- COGs/correlations_genome_est.R
+++ COGs/correlations_genome_est.R
@@ -2,12 +2,16 @@ library(ggplot2)
 library(reshape2)
 library(dplyr)
 library(stats)
+library(tidyr)
 
+##############
+#PREPARE DATA#
+##############
 #print working directory for future references
 wd=print(getwd())
 
 #read in data
-data=read.delim(file = paste(wd, "/data/COG_data.txt", sep=""))
+data=read.delim(file = paste(wd, "/data/abundance_cog_both_est.tab.xls", sep=""))
 
 #make COG ID's the row names
 row.names(data)=data$COGID
@@ -32,51 +36,70 @@ colnames(info)=c("Site", "Temp", "As", "History")
 data$COGID=rownames(data)
 tidy=melt(data, id.vars="COGID", variable.name="Site", value.name = "Abundance")
 
-#fix sit id naming in cog data
-tidy$Site=gsub("C", "Cen", tidy$Site)
-
 #join data with info
-joined=inner_join(size, info)
-final=inner_join(tidy, joined, by="Site")
+final=size %>%
+  inner_join(info) %>%
+  inner_join(.,tidy, by="Site")
 
 #set Abundance to numeric
 final$Abundance=as.numeric(final$Abundance)
 
-#normalize abundance
+################
+#NORMALIZE DATA#
+################
+#normalize abundance genome equivalents
 final$norm=final$Abundance/final$GE
 
-#wide dataset
-cast=acast(final, Site ~ COGID, id=c("Site", "COGID"), value.var = "norm")
+##normalize abundance to total COG counts
+#calculate toal COG count per site
+total <- final %>%
+  group_by(Site) %>%
+  summarise(N=length(Abundance), 
+            total=sum(Abundance))
 
-#correlation
-corr=cor(joined$Temp, cast, method = "pearson")
+#add total data to final df
+final <- final %>%
+  left_join(total, by = "Site") 
 
-#tidy correlated data
-slim=melt(corr,value.name = "R")
-slim=slim[,-1]
-colnames(slim)=c("COGID", "R")
+#normalize abundance to total 
+final$anorm=final$Abundance/final$total
 
-#annotate correlated data
-funct$COGID=rownames(funct)
-slim=inner_join(slim, funct, by="COGID")
+#########################################
+#CALCULATE CORRELATIONS WITH TEMPERATURE#
+#########################################
+#widen GE-normalized dataset
+norm=acast(final, Site ~ COGID, id=c("Site", "COGID"), value.var = "norm")
 
-#subset based on correl
-neg=slim[which(slim$R<0),]
-mpos=subset(slim, slim$R>0 & slim$R<0.8)
-vpos=slim[which(slim$R>0.9),]
+#widen total-normalized dataset
+anorm=acast(final, Site ~ COGID, id=c("Site", "COGID"), value.var = "anorm")
 
-#subset initial data based on correl
-neg.full=final[which(final$COGID %in% neg$COGID),]
-mpos.full=final[which(final$COGID %in% mpos$COGID),]
-vpos.full=final[which(final$COGID %in% vpos$COGID),]
+#correlation based on GE-normalized dataset
+corr=cor(info$Temp, norm, method = "pearson")
 
-##examine single copy genes
+#correlation based on total-normalized dataset
+acorr=cor(info$Temp, anorm, method = "pearson")
+
+#tidy GE-correlated data
+tcorr=melt(corr,value.name = "R")
+tcorr=tcorr[,-1]
+colnames(tcorr)=c("COGID", "GER")
+
+#tidy total-correlated data
+tacorr=melt(acorr,value.name = "R")
+tacorr=tacorr[,-1]
+colnames(tacorr)=c("COGID", "totR")
+
+#join correl data
+corr=inner_join(tcorr, tacorr, by = "COGID")
+
+###########################
+#EXAMINE SINGLE COPY GENES#
+###########################
 #list genes
 genes=c("COG0016","COG0048","COG0049","COG0051","COG0052","COG0072","COG0080","COG0081","COG0087","COG0088", "COG0090", "COG0091","COG0092","COG0093","COG0094","COG0096","COG0097","COG0098","COG0099","COG0100","COG0103","COG0127","COG0149","COG0164","COG0184","COG0185","COG0186","COG0197","COG0200","COG0244","COG0256","COG0343","COG0481","COG0504","COG0532","COG0533","COG0541")
 
 #list genes used by microbe census
 mc.genes <- c("COG0052", "COG0081", "COG0532", "COG0091", 'COG0088', 'COG0090', "COG0103", 'COG0087', "COG0072", "COG0093", "COG0098", "COG0185", "COG0049", "COG0197", "COG0099", "COG0016", "COG0200", "COG0097", "COG0080", "COG0094", "COG0048", "COG0092", "COG0100", "COG0244", "COG0096", "COG0256", "COG0184", "COG0186", "COG0102", "COG0198")
-
 
 #select COGs Tringe lists as single copy
 single=final[which(final$COGID %in% genes),]
@@ -84,32 +107,51 @@ single=final[which(final$COGID %in% genes),]
 #group
 single=group_by(single, COGID)
 
-#average count
+#average count of GE-normalized cogs
 avglength=summarise(single, n=length(norm), average=mean(norm))
 
-#calculate odds ratio
+#average count of total-normalized cogs
+aavglength=summarise(single, n=length(anorm), average=mean(anorm))
+
+#calculate odds ratio GE-normalized cogs
 single$odds.ratio=single$norm/avglength$average
 
+#calculate odds ratio total-normalized cogs
+single$a.odds.ratio=single$anorm/aavglength$average
 
-#plot odds ratios for each COG (single copy)
+#plot odds ratios for each COG (single copy) (GE-normalized)
 (or=ggplot(single, aes(x=COGID, y=odds.ratio)) +
-  geom_boxplot() +
-  geom_jitter(height = 0, aes(color=Site, shape=History), size=1.5) +
-  ylim(0,2) +
-  xlab("Tringe paper COGS") +
-  coord_flip())
+    geom_boxplot() +
+    geom_jitter(height = 0, aes(color=Site, shape=History), size=1.5) +
+    ylim(0,2) +
+    xlab("Tringe paper COGS") +
+    coord_flip())
 
 #save plot
 ggsave(or, filename = paste(wd, "/figures/GE.norm.odds.ratios.png", sep=""))
 
-#plot odds ratio v. temperature 
+#plot odds ratios for each COG (single copy) (total-normalized)
+(or=ggplot(single, aes(x=COGID, y=a.odds.ratio)) +
+    geom_boxplot() +
+    geom_jitter(height = 0, aes(color=Site, shape=History), size=1.5) +
+    ylim(0,2) +
+    xlab("Tringe paper COGS") +
+    coord_flip())
+
+#plot odds ratio v. temperature (GE-normalized)
 (or.temp=ggplot(single, aes(x=Temp, y=odds.ratio, color=COGID)) +
-  geom_point(size=1) +
-  stat_smooth(method=lm, alpha=0.01, size=0.25) +
-  ylim(0.25, 1.6))
+    geom_point(size=1) +
+    stat_smooth(method=lm, alpha=0.01, size=0.25) +
+    ylim(0.25, 1.6))
 
 #save plot
 ggsave(or.temp, filename = paste(wd, "/figures/GE.norm.odds.ratio.v.temp.png", sep=""))
+
+#plot odds ratio v. temperature (total-normalized)
+(or.temp=ggplot(single, aes(x=Temp, y=a.odds.ratio, color=COGID)) +
+    geom_point(size=1) +
+    stat_smooth(method=lm, alpha=0.01, size=0.25) +
+    ylim(0.25, 1.6))
 
 #check correlations
 single.r=slim[which(slim$COGID %in% genes),]
@@ -126,7 +168,6 @@ mc.avglength=summarise(census, n=length(norm), average=mean(norm))
 
 #calculate odds ratio
 census$odds.ratio=census$norm/mc.avglength$average
-
 
 #plot odds ratios for each COG (single copy)
 (mc.or=ggplot(census, aes(x=COGID, y=odds.ratio)) +
@@ -148,12 +189,86 @@ ggsave(mc.or, filename = paste(wd, "/figures/mc.GE.norm.odds.ratios.png", sep=""
 #save plot
 ggsave(mc.or.temp, filename = paste(wd, "/figures/mc.GE.norm.odds.ratio.v.temp.png", sep=""))
 
+#######################
+#FUNCTIONAL ASSESSMENT#
+#######################
 
+#read in COGIDs with functional letter
+cogid=read.delim(file = paste(wd, "/data/cognames2003-2014.txt", sep=""))
 
+#add column names
+colnames(cogid)=c("COGID", "func", "name")
 
+#tidy COGs with multiple functional groups
+cogid= cogid %>%
+  separate(func, c("func", "func2"), sep=1) %>%
+  separate(func2, c("func2", "func3"), sep=1) %>%
+  melt(id.vars = c("COGID", "name"), measure.vars = c("func", "func2", "func3")) 
 
+#remove unnecessary function column
+cogid <- cogid[-3]
 
+#remove rows with empty values
+cogid <- cogid[-which(cogid$value == ""),]
 
+#change column names of cogid
+colnames(cogid) <- c("COGID", "name", "func")
+
+#join cogid data with data
+final <- left_join(final, cogid, by = "COGID")
+
+#read in functional letter key
+funct.key=read.delim(file = paste(wd, "/data/fun2003-2014.txt", sep=""))
+colnames(funct.key)=c("func", "func.name")
+
+#annotate data with COGID functional groups
+final <- final %>%
+  left_join(funct.key, by = "func")
+
+#group appropriately
+final <- group_by(final, func, Site)
+
+#plot
+(totplot <- ggplot(final, aes(x = func.name, y = anorm, color = History)) +
+    geom_bar(stat = "identity", position = "dodge") +
+    coord_flip())
+
+ggplot(final, aes(x = func.name, y = anorm, color = History)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  coord_flip()
+
+(geplot <- ggplot(final, aes(x = func.name, y = norm, fill = History)) +
+    geom_bar(stat = "identity", position = "dodge") +
+    coord_flip())
+
+###########################
+#EXAMINE %GENE VS. %GENOME#
+###########################
+#annotated temperature correlation data
+corr.annotated <- corr %>%
+  inner_join(., cogid, by = "COGID") %>%
+  inner_join(., funct.key, by = "func")
+
+#group genes based on %gene in total and % gene in genomes
+pp <- corr.annotated[which(corr.annotated$GER > 0.6 & corr.annotated$totR > 0.6),]
+pp <- cbind(pp, designation="pp")
+pn <- corr.annotated[which(corr.annotated$GER < 0.6 & corr.annotated$GER > c(-0.6) & corr.annotated$totR > 0.6),]
+pn <- cbind(pn, designation="pn")
+pN <- corr.annotated[which(corr.annotated$GER < c(-0.6) & corr.annotated$totR > 0.6),]
+pN <- cbind(pN, designation="pN")
+nN <- corr.annotated[which(corr.annotated$GER < c(-0.6) & corr.annotated$totR < 0.6 & corr.annotated$totR > c(-0.6)),]
+nN <- cbind(nN, designation="nN")
+NN <- corr.annotated[which(corr.annotated$GER < c(-0.6) & corr.annotated$totR < c(-0.6)),]
+NN <- cbind(NN, designation="NN")
+nn <- corr.annotated[which(corr.annotated$GER > c(-0.6) & corr.annotated$GER < 0.6 & corr.annotated$totR < 0.6 & corr.annotated$totR > c(-0.6)),]
+nn <- cbind(nn, designation="nn")
+desi <- rbind(pp, pn, pN, nN, NN, nn)
+
+mobility <- desi[which(desi$func.name == "Mobilome: prophages, transposons"),]
+ggplot(desi, aes(x = name, fill = designation)) +
+  geom_bar(position = "dodge") +
+  coord_polar() +
+  facet_grid(~func.name)
 
 
 
