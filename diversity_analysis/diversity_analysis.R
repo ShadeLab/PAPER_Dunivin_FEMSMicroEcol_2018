54467ffaf26252216b8b19a8752450fa90a02e01
dunivint <dunivint@Taylors-MacBook-Pro-5.local>
2017-05-11 11:37:05 -0400

    combined diversity analyses into 1 workflow

--- diversity_analysis/diversity_analysis.R
+++ diversity_analysis/diversity_analysis.R
@@ -1,11 +1,100 @@
 #load dependencies 
 library(phyloseq)
+library(ape)
+library(biomformat)
 library(vegan)
 library(tidyverse)
+library(reshape2)
 
 #print working directory for future references
+#note the GitHub directory for this script is as follows
+#https://github.com/ShadeLab/Xander_arsenic/tree/master/diversity_analysis
 wd=print(getwd())
 
+##################################
+#PHYLUM_LEVEL_RESPONSES_WITH_RPLB#
+##################################
+
+#temporarily change working directory to data to bulk load files
+setwd(paste(wd, "/data", sep = ""))
+
+#read in abundance data
+names=list.files(pattern="*rplB_45_taxonabund.txt")
+data <- do.call(rbind, lapply(names, function(X) {
+  data.frame(id = basename(X), read.csv(X))}))
+
+#move back up a directory to proceed with analysis
+setwd("../")
+wd <- print(getwd())
+
+#remove rows that include lineage match name (only need taxon)
+#aka remove duplicate data
+data <- data[!grepl(";", data$Taxon.Abundance.Fraction.Abundance),]
+data <- data[!grepl("Lineage", data$Taxon.Abundance.Fraction.Abundance),]
+
+#split columns 
+data <- data %>%
+  separate(col = id, into = c("Site", "junk"), sep = 5, remove = TRUE) %>%
+  separate(col = Taxon.Abundance.Fraction.Abundance, 
+           into = c("Taxon", "Abundance", "Fraction.Abundance"), 
+           sep = "\t") %>%
+  select(-junk) %>%
+  group_by(Site)
+
+#make sure abundance and fraction abundance are numbers
+#R will think it's a char since it started w taxon name
+data$Fraction.Abundance <- as.numeric(data$Fraction.Abundance)
+data$Abundance <- as.numeric(data$Abundance)
+
+#double check that all fraction abundances = 1
+#slightly above or below is okay (Xander rounds)
+summarised <- data %>%
+  summarise(N = length(Site), Total = sum(Fraction.Abundance))
+
+#change site from "cen" to "Cen" so it matches metadata
+data$Site <- gsub("cen", "Cen", data$Site)
+
+#decast for abundance check
+dcast=acast(data, Taxon ~ Site, value.var = "Fraction.Abundance")
+
+#call na's zeros
+dcast[is.na(dcast)] =0
+
+#order based on abundance
+order.dcast=dcast[order(rowSums(dcast),decreasing=TRUE),]
+
+#melt data
+melt=melt(order.dcast,id.vars=row.names(order.dcast), variable.name= "Site", value.name="Fraction.Abundance" )
+
+#adjust colnames of melt
+colnames(melt)=c("Taxon", "Site", "Fraction.Abundance")
+
+#read in metadata
+meta=read.delim(file = paste(wd, "/data/Centralia_full_map.txt", sep=""), sep=" ")
+
+#join metadata with regular data
+joined=inner_join(melt, meta, by="Site")
+
+#average by fire history
+#group data
+grouped=group_by(joined, Taxon, Classification)
+
+#calculate
+history=summarise(grouped, N=length(Fraction.Abundance), Average=mean(Fraction.Abundance))
+
+#plot
+(phylum.plot=(ggplot(history, aes(x=Taxon, y=Average)) +
+                geom_point(size=2) +
+                facet_wrap(~Classification, ncol = 1) +
+                labs(x="Phylum", y="Mean relative abundance")+
+                theme(axis.text.x = element_text(angle = 90, size = 12, hjust=0.95,vjust=0.2))))
+
+#save plot
+ggsave(phylum.plot, filename = paste(wd, "/figures/phylum.responses.png", sep=""), width = 5, height = 5)
+
+#########################
+#RplB DIVERSITY ANALYSIS#
+#########################
 #read in metadata
 meta=data.frame(read.delim(file = paste(wd, "/data/Centralia_JGI_map.txt", sep=""), sep=" ", header=TRUE))
 
@@ -135,4 +224,472 @@ phylo=merge_phyloseq(tree, rare, metad)
 
 ggsave(tree.plot, filename = paste(wd, "/figures/rplb.tree.png", sep=""), 
        width = 10, height = 40)
+
+#########################
+#ArsB DIVERSITY ANALYSIS#
+#########################
+
+#read in metadata
+meta=data.frame(read.delim(file = paste(wd, "/data/Centralia_JGI_map.txt", sep=""), sep=" ", header=TRUE))
+
+#remove Cen16 from metadata since we don't have arsB info yet
+meta=meta[which(meta$Site == "Cen17" | meta$Site == "Cen10"),]
+meta$Site <- as.character(meta$Site)
+
+#read in distance matrix
+arsB=read.delim(file = paste(wd, "/data/arsB_rformat_dist_0.03.txt", sep=""))
+
+#add row names back
+rownames(arsB)=arsB[,1]
+
+#remove first column
+arsB=arsB[,-1]
+
+#make data matrix
+arsB=data.matrix(arsB)
+
+#remove first column
+arsB=arsB[,-1]
+
+#call metadata sample data
+metad=meta[-1]
+rownames(metad)=meta$Site
+metad=sample_data(metad)
+
+#otu table
+otu=otu_table(arsB, taxa_are_rows = FALSE)
+
+#see rarefaction curve
+rarecurve(otu, step=5, col = c("black", "darkred", "forestgreen", "orange", "blue", "yellow", "hotpink"), label = FALSE)
+
+#rarefy
+rare=rarefy_even_depth(otu, sample.size = min(sample_sums(otu)), rngseed = TRUE)
+
+#check curve
+rarecurve(rare, step=5, col = c("black", "darkred", "forestgreen", "orange", "blue", "yellow", "hotpink"), label = TRUE)
+
+##make biom for phyloseq
+phylo=merge_phyloseq(rare, metad)
+
+#plot phylo richness
+(richness=plot_richness(phylo, x="Sample", shape="Classification", color = "SoilTemperature_to10cm"))
+
+#save plot 
+ggsave(richness, filename = paste(wd, "/figures/arsB.richness.png", sep=""), 
+       width = 15)
+
+#calculate evenness
+s=specnumber(rare)
+h=diversity(rare, index="shannon")
+plieou=h/log(s)
+
+#save evenness number
+write.table(plieou, file = paste(wd, "/output/arsB.evenness.txt", sep=""))
+
+#make plieou a dataframe for plotting
+plieou=data.frame(plieou)
+
+#add site column to evenness data
+plieou$Site=rownames(plieou)
+
+#merge evenness information with fire classification
+plieou=inner_join(plieou, meta)
+
+#make color pallette
+GnYlOrRd=colorRampPalette(colors=c("green", "yellow", "orange","red"), bias=2)
+
+#plot evenness by fire classification
+(evenness <- ggplot(plieou, aes(x = Classification, y = plieou)) +
+    geom_point(aes(color = SoilTemperature_to10cm), size=3) +
+    scale_color_gradientn(colours=GnYlOrRd(5), guide="colorbar", 
+                          guide_legend(title="Temperature (°C)")) +
+    ylab("Evenness") +
+    xlab("Fire classification") +
+    theme_bw(base_size = 12))
+
+#save evenness plot
+ggsave(evenness, filename = paste(wd, "/figures/arsB.evenness.png", sep=""), 
+       width = 7, height = 5)
+
+#make an output of total gene count per site
+arsB.gcounts=rowSums(arsB)
+
+#make object phylo with tree and biom info
+tree <- read.tree(file = paste(wd, "/data/arsB_0.03_tree.nwk", sep=""))
+tree <- phy_tree(tree)
+
+#merge
+phylo=merge_phyloseq(tree, rare, metad)
+
+#plot tree
+(tree.plot <- plot_tree(phylo, color = "SoilTemperature_to10cm", size = "abundance",
+                        shape = "Classification", label.tips="taxa_names", 
+                        text.size=2, ladderize="left", base.spacing = 0.03, sizebase = 2) +
+    theme(legend.position = "right", legend.title = element_text(size=11),
+          legend.key =element_blank()) +
+    scale_color_gradientn(colours=GnYlOrRd(5), guide="colorbar", 
+                          guide_legend(title="Temperature (°C)")))
+
+plot_bar(phylo, fill = "Classification")
+
+##############################################
+#EXAMINE TAXON ABUNDANCE DIFFERENCES FOR ARSB#
+##############################################
+#read in each file (one per site)
+cen10 <- read_delim(file = paste(wd, "/data/arsB_taxonabund_cen10.txt", sep = ""), 
+                    col_names = TRUE, delim = "\t")
+cen17 <- read_delim(file = paste(wd, "/data/arsB_taxonabund_cen17.txt", sep = ""), 
+                    col_names = TRUE, delim = "\t")
+
+#make column for organism name
+cen10 <- cen10 %>%
+  mutate(Site = "cen10", Gene = "arsB", Census = 8744.20, rplB = 3040) %>%
+  mutate(Normalized.Abundance.rplB = Abundance / rplB, 
+         Normalized.Abundance.census = Abundance / Census) %>%
+  separate(Taxon, into = c("coded_by", "organism"), sep = ",organism=") %>%
+  separate(organism, into = c("organism", "definition"), sep = ",definition=")
+
+cen17 <- cen17 %>%
+  mutate(Site = "cen17", Gene = "arsB", Census = 3867.45, rplB = 4928) %>%
+  mutate(Normalized.Abundance.rplB = Abundance / rplB, 
+         Normalized.Abundance.census = Abundance / Census) %>%  
+  separate(Taxon, into = c("coded_by", "organism"), sep = ",organism=") %>%
+  separate(organism, into = c("organism", "definition"), sep = ",definition=")
+
+#join data together
+arsB <- rbind(cen17, cen10)
+arsB.high <- arsB[which(arsB$Abundance > 10),]
+
+#plot data
+ggplot(arsB, aes(x = Site, y = Normalized.Abundance.rplB)) +
+  geom_bar(stat = "identity")
+
+ggplot(arsB, aes(x = Site, y = Normalized.Abundance.census)) +
+  geom_bar(stat = "identity")
+
+ggplot(arsB, aes(x = organism, y = Normalized.Abundance.census)) +
+  geom_point(aes(color = Site)) +
+  coord_flip()
+
+ggplot(arsB, aes(x = organism, y = Normalized.Abundance.rplB)) +
+  geom_point(aes(color = Site)) +
+  coord_flip()
+
+#########################
+#ACR3 DIVERSITY ANALYSIS#
+#########################
+
+#read in metadata
+meta=data.frame(read.delim(file = paste(wd, "/data/Centralia_JGI_map.txt", sep=""), sep=" ", header=TRUE))
+
+#remove Cen16 from metadata since we don't have acr3 info yet
+meta=meta[which(meta$Site == "Cen14" | meta$Site == "Cen03"),]
+meta$Site <- as.character(meta$Site)
+
+#read in distance matrix
+acr3=read.delim(file = paste(wd, "/data/acr3_rformat_dist_0.03.txt", sep=""))
+
+#add row names back
+rownames(acr3)=acr3[,1]
+
+#remove first column
+acr3=acr3[,-1]
+
+#make data matrix
+acr3=data.matrix(acr3)
+
+#remove first column
+acr3=acr3[,-1]
+
+#call metadata sample data
+metad=meta[-1]
+rownames(metad)=meta$Site
+metad=sample_data(metad)
+
+#otu table
+otu=otu_table(acr3, taxa_are_rows = FALSE)
+
+#see rarefaction curve
+rarecurve(otu, step=5, col = c("black", "darkred", "forestgreen", "orange", "blue", "yellow", "hotpink"), label = FALSE)
+
+#rarefy
+rare=rarefy_even_depth(otu, sample.size = min(sample_sums(otu)), rngseed = TRUE)
+
+#check curve
+rarecurve(rare, step=5, col = c("black", "darkred", "forestgreen", "orange", "blue", "yellow", "hotpink"), label = TRUE)
+
+##make biom for phyloseq
+phylo=merge_phyloseq(rare, metad)
+
+#plot phylo richness
+(richness=plot_richness(phylo, x="Sample", shape="Classification", color = "SoilTemperature_to10cm"))
+
+#save plot 
+ggsave(richness, filename = paste(wd, "/figures/acr3.richness.png", sep=""), 
+       width = 15)
+
+#calculate evenness
+s=specnumber(rare)
+h=diversity(rare, index="shannon")
+plieou=h/log(s)
+
+#save evenness number
+write.table(plieou, file = paste(wd, "/output/acr3.evenness.txt", sep=""))
+
+#make plieou a dataframe for plotting
+plieou=data.frame(plieou)
+
+#add site column to evenness data
+plieou$Site=rownames(plieou)
+
+#merge evenness information with fire classification
+plieou=inner_join(plieou, meta)
+
+#make color pallette
+GnYlOrRd=colorRampPalette(colors=c("green", "yellow", "orange","red"), bias=2)
+
+#plot evenness by fire classification
+(evenness <- ggplot(plieou, aes(x = Classification, y = plieou)) +
+    geom_point(aes(color = SoilTemperature_to10cm), size=3) +
+    scale_color_gradientn(colours=GnYlOrRd(5), guide="colorbar", 
+                          guide_legend(title="Temperature (°C)")) +
+    ylab("Evenness") +
+    xlab("Fire classification") +
+    theme_bw(base_size = 12))
+
+#save evenness plot
+ggsave(evenness, filename = paste(wd, "/figures/acr3.evenness.png", sep=""), 
+       width = 7, height = 5)
+
+#make an output of total gene count per site
+acr3.gcounts=rowSums(acr3)
+
+#make object phylo with tree and biom info
+tree <- read.tree(file = paste(wd, "/data/acr3_0.03_tree.nwk", sep=""))
+tree <- phy_tree(tree)
+
+#merge
+phylo=merge_phyloseq(tree, rare, metad)
+
+#plot tree
+(tree.plot <- plot_tree(phylo, color = "SoilTemperature_to10cm", size = "abundance",
+                        shape = "Classification", label.tips="taxa_names", 
+                        text.size=2, ladderize="left", base.spacing = 0.03) +
+    theme(legend.position = "right", legend.title = element_text(size=11),
+          legend.key =element_blank()) +
+    scale_color_gradientn(colours=GnYlOrRd(5), guide="colorbar", 
+                          guide_legend(title="Temperature (°C)")))
+
+plot_bar(phylo, fill = "Classification")
+
+##############################################
+#EXAMINE TAXON ABUNDANCE DIFFERENCES FOR ACR3#
+##############################################
+
+#read in each file (one per site)
+cen14 <- read_delim(file = paste(wd, "/data/acr3_taxonabund_cen14.txt", sep = ""), 
+                    col_names = TRUE, delim = "\t")
+cen03 <- read_delim(file = paste(wd, "/data/acr3_taxonabund_cen03.txt", sep = ""), 
+                    col_names = TRUE, delim = "\t")
+
+#make column for organism name
+cen14 <- cen14 %>%
+  mutate(Site = "Cen14", Gene = "acr3", Census = 5243, rplB = 1974.53) %>%
+  mutate(Normalized.Abundance.rplB = Abundance / rplB, 
+         Normalized.Abundance.census = Abundance / Census) %>%
+  separate(Taxon, into = c("coded_by", "organism"), sep = ",organism=") %>%
+  separate(organism, into = c("organism", "definition"), sep = ",definition=")
+
+cen03 <- cen03 %>%
+  mutate(Site = "Cen03", Gene = "acr3", Census = 4316, rplB = 1347.908) %>%
+  mutate(Normalized.Abundance.rplB = Abundance / rplB, 
+         Normalized.Abundance.census = Abundance / Census) %>%  
+  separate(Taxon, into = c("coded_by", "organism"), sep = ",organism=") %>%
+  separate(organism, into = c("organism", "definition"), sep = ",definition=")
+
+#join data together
+acr3 <- rbind(cen03, cen14)
+acr3.high <- acr3[which(acr3$Abundance > 10),]
+
+#plot data
+ggplot(acr3, aes(x = Site, y = Normalized.Abundance.rplB)) +
+  geom_bar(stat = "identity")
+
+ggplot(acr3, aes(x = Site, y = Normalized.Abundance.census)) +
+  geom_bar(stat = "identity")
+
+ggplot(acr3, aes(x = organism, y = Normalized.Abundance.census)) +
+  geom_point(aes(color = Site)) +
+  coord_flip()
+
+ggplot(acr3, aes(x = organism, y = Normalized.Abundance.rplB)) +
+  geom_point(aes(color = Site)) +
+  coord_flip()
+
+#########################
+#ACR3 DIVERSITY ANALYSIS#
+#########################
+
+#read in metadata
+meta=data.frame(read.delim(file = paste(wd, "/data/Centralia_JGI_map.txt", sep=""), sep=" ", header=TRUE))
+
+#remove Cen16 from metadata since we don't have aioA info yet
+meta=meta[which(meta$Site == "Cen07" | meta$Site == "Cen10"),]
+meta$Site <- as.character(meta$Site)
+
+#read in distance matrix
+aioA=read.delim(file = paste(wd, "/data/aioA_rformat_dist_0.03.txt", sep=""))
+
+#add row names back
+rownames(aioA)=aioA[,1]
+
+#remove first column
+aioA=aioA[,-1]
+
+#make data matrix
+aioA=data.matrix(aioA)
+
+#remove first column
+aioA=aioA[,-1]
+
+
+#call metadata sample data
+metad=meta[-1]
+rownames(metad)=meta$Site
+metad=sample_data(metad)
+
+#otu table
+otu=otu_table(aioA, taxa_are_rows = FALSE)
+
+#see rarefaction curve
+rarecurve(otu, step=5, col = c("black", "darkred", "forestgreen", "orange", "blue", "yellow", "hotpink"), label = FALSE)
+
+#rarefy
+rare=rarefy_even_depth(otu, sample.size = min(sample_sums(otu)), rngseed = TRUE)
+
+#check curve
+rarecurve(rare, step=5, col = c("black", "darkred", "forestgreen", "orange", "blue", "yellow", "hotpink"), label = TRUE)
+
+##make biom for phyloseq
+phylo=merge_phyloseq(rare, metad)
+
+#plot phylo richness
+(richness=plot_richness(phylo, x="Sample", shape="Classification", color = "SoilTemperature_to10cm"))
+
+#save plot 
+ggsave(richness, filename = paste(wd, "/figures/aioA.richness.png", sep=""), 
+       width = 15)
+
+#calculate evenness
+s=specnumber(rare)
+h=diversity(rare, index="shannon")
+plieou=h/log(s)
+
+#save evenness number
+write.table(plieou, file = paste(wd, "/output/aioA.evenness.txt", sep=""))
+
+#make plieou a dataframe for plotting
+plieou=data.frame(plieou)
+
+#add site column to evenness data
+plieou$Site=rownames(plieou)
+
+#merge evenness information with fire classification
+plieou=inner_join(plieou, meta)
+
+#make color pallette
+GnYlOrRd=colorRampPalette(colors=c("green", "yellow", "orange","red"), bias=2)
+
+#plot evenness by fire classification
+(evenness <- ggplot(plieou, aes(x = Classification, y = plieou)) +
+    geom_point(aes(color = SoilTemperature_to10cm), size=3) +
+    scale_color_gradientn(colours=GnYlOrRd(5), guide="colorbar", 
+                          guide_legend(title="Temperature (°C)")) +
+    ylab("Evenness") +
+    xlab("Fire classification") +
+    theme_bw(base_size = 12))
+
+#save evenness plot
+ggsave(evenness, filename = paste(wd, "/figures/aioA.evenness.png", sep=""), 
+       width = 7, height = 5)
+
+#make an output of total gene count per site
+aioA.gcounts=rowSums(aioA)
+
+#make object phylo with tree and biom info
+tree <- read.tree(file = paste(wd, "/data/aioA_0.03_tree.nwk", sep=""))
+tree <- phy_tree(tree)
+
+#merge
+phylo=merge_phyloseq(tree, rare, metad)
+
+#plot tree
+(tree.plot <- plot_tree(phylo, color = "SoilTemperature_to10cm", size = "abundance",
+                        shape = "Classification", label.tips="taxa_names", 
+                        text.size=2, ladderize="left", base.spacing = 0.03, sizebase = 2) +
+    theme(legend.position = "right", legend.title = element_text(size=11),
+          legend.key =element_blank()) +
+    scale_color_gradientn(colours=GnYlOrRd(5), guide="colorbar", 
+                          guide_legend(title="Temperature (°C)")))
+
+plot_bar(phylo, fill = "Classification")
+
+##############################################
+#EXAMINE TAXON ABUNDANCE DIFFERENCES FOR AIOA#
+##############################################
+
+#read in each file (one per site)
+cen10 <- read_delim(file = paste(wd, "/data/aioA_taxonabund_cen10.txt", sep = ""), 
+                    col_names = TRUE, delim = "\t")
+cen07 <- read_delim(file = paste(wd, "/data/aioA_taxonabund_cen07.txt", sep = ""), 
+                    col_names = TRUE, delim = "\t")
+
+#make column for organism name
+cen10 <- cen10 %>%
+  mutate(Site = "cen10", Gene = "aioA", Census = 8744.20, rplB = 3040) %>%
+  mutate(Normalized.Abundance.rplB = Abundance / rplB, 
+         Normalized.Abundance.census = Abundance / Census) %>%
+  separate(Taxon, into = c("coded_by", "organism"), sep = ",organism=") %>%
+  separate(organism, into = c("organism", "definition"), sep = ",definition=")
+
+cen07 <- cen07 %>%
+  mutate(Site = "cen07", Gene = "aioA", Census = 3584.52, rplB = 1347.908) %>%
+  mutate(Normalized.Abundance.rplB = Abundance / rplB, 
+         Normalized.Abundance.census = Abundance / Census) %>%  
+  separate(Taxon, into = c("coded_by", "organism"), sep = ",organism=") %>%
+  separate(organism, into = c("organism", "definition"), sep = ",definition=")
+
+#join data together
+aioA <- rbind(cen07, cen10)
+aioA.high <- aioA[which(aioA$Abundance > 10),]
+
+#plot data
+ggplot(aioA, aes(x = Site, y = Normalized.Abundance.rplB)) +
+  geom_bar(stat = "identity")
+
+ggplot(aioA, aes(x = Site, y = Normalized.Abundance.census)) +
+  geom_bar(stat = "identity")
+
+ggplot(aioA, aes(x = organism, y = Normalized.Abundance.census)) +
+  geom_point(aes(color = Site)) +
+  coord_flip()
+
+ggplot(aioA, aes(x = organism, y = Normalized.Abundance.rplB)) +
+  geom_point(aes(color = Site)) +
+  coord_flip()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
