0189a7dbd4d73266fd706115f99215859d981420
dunivint <dunivint@Taylors-MacBook-Pro-5.local>
2017-06-13 12:05:54 -0400

    ADDED ACR3/ARSM ABUNDANCE INFO FOR TREES

--- phylogenetic_analysis/tree_setup.R
+++ phylogenetic_analysis/tree_setup.R
@@ -6,12 +6,12 @@ setwd("/Users/dunivint/Documents/GitHubRepos/Xander_arsenic/diversity_analysis/"
 wd <- paste(getwd())
 
 #read in label data
-labels <- read_delim("/Users/dunivint/Documents/GitHubRepos/Xander_arsenic/phylogenetic_analysis/aioA/aioA_0.1_labels.txt", delim = ",", col_names = FALSE)
+labels <- read_delim("/Users/dunivint/Documents/GitHubRepos/Xander_arsenic/phylogenetic_analysis/acr3/acr3_0.1_labels.txt", delim = ",", col_names = FALSE)
 colnames(labels) <- c("Label", "OTU")
 labels$OTU <- gsub(" ", "", labels$OTU)
 
 #read in OTU table 
-table <- read.delim("/Users/dunivint/Documents/GitHubRepos/Xander_arsenic/phylogenetic_analysis/aioA/rformat_dist_0.1.txt", header = TRUE)
+table <- read.delim("/Users/dunivint/Documents/GitHubRepos/Xander_arsenic/phylogenetic_analysis/acr3/rformat_dist_0.1.txt", header = TRUE)
 
 #read in census data
 census <- read_delim(file = paste(wd, "/data/microbe_census.txt", sep = ""),
@@ -32,7 +32,7 @@ table.census <- table %>%
   left_join(census, by = "Site")
 
 #normalize data
-table.normalized <- 100*table.census[2:44]/table.census$GE
+table.normalized <- 100*table.census[2:398]/table.census$GE
 
 #add back site information
 table.normalized <- cbind(table.census$Site, table.normalized)
@@ -45,21 +45,29 @@ table.normalized <- table.normalized[,-1]
 table.normalized <- table.normalized %>%
   left_join(meta, by = "Site") %>%
   arrange(Site, desc(SoilTemperature_to10cm)) %>%
-  select(OTU_01:Site, SoilTemperature_to10cm)
+  select(OTU_001:Site, SoilTemperature_to10cm)
 
 
 table.normalized$Site <- factor(table.normalized$Site, 
                                 levels = table.normalized$Site[order(meta$SoilTemperature_to10cm)])
 
 #transform otu table
-table.normalized.t = setNames(data.frame(t(table.normalized[,-c(44,45)])), 
-                              table.normalized[,44])
+table.normalized.t = setNames(data.frame(t(table.normalized[,-c(398,399)])), 
+                              table.normalized[,398])
 #make OTUs a column
 table.normalized.t$OTU <- rownames(table.normalized.t)
 
 #add name to first column
 table.normalized.t$OTU <- gsub(" ", "", table.normalized.t$OTU)
-            
+
+#fix OTU names with weird # before them
+table.normalized.t$OTU <- gsub(".OTU_", "OTU_", table.normalized.t$OTU)
+
+#fix label names
+labels$OTU <- gsub("TU_0003", "OTU_0003", labels$OTU)
+labels$OTU <- gsub("TU_0005", "OTU_0005", labels$OTU)
+labels$OTU <- gsub(".OTU_", "OTU_", labels$OTU)
+
 #join abundance with name
 label.abund <- left_join(labels, table.normalized.t, by = "OTU")
 
@@ -67,7 +75,7 @@ label.abund <- left_join(labels, table.normalized.t, by = "OTU")
 label.abund <- select(label.abund, -OTU)
 
 #save file
-write.csv(label.abund, "/Users/dunivint/Documents/ShadeLab/Experiments/Xander/aioA_abund_label.csv", row.names = FALSE)
+write.csv(label.abund, "/Users/dunivint/Documents/GitHubRepos/Xander_arsenic/phylogenetic_analysis/acr3/acr3_abund_label.csv", row.names = FALSE)
 
 
 
