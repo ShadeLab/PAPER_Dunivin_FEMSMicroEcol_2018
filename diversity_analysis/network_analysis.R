d7cba5f9d91bc77ac758a3bf764eec9f07304cff
dunivint <dunivint@Taylors-MacBook-Pro-5.local>
2017-06-16 21:31:10 -0400

    added network analysis with rplB

--- diversity_analysis/network_analysis.R
+++ diversity_analysis/network_analysis.R
@@ -1,13 +1,16 @@
 library(vegan)
-library(igraph)
-library(Hmisc)
 library(psych)
 library(tidyverse)
+library(qgraph)
 
 #print working directory for future references
 #note the GitHub directory for this script is as follows
 #https://github.com/ShadeLab/Xander_arsenic/tree/master/diversity_analysis
 wd <- print(getwd())
+
+#read in metadata
+meta <- data.frame(read.delim(paste(wd, "/data/Centralia_JGI_map.txt", 
+                                    sep=""), sep=" ", header=TRUE))
 
 #write OTU naming function
 naming <- function(file) {
@@ -15,23 +18,22 @@ naming <- function(file) {
 }
 
 #temporarily change working directories
-setwd(paste(wd, "/data/0.03_clust", sep = ""))
+setwd(paste(wd, "/data/0.1_clust", sep = ""))
 
 #list filenames of interest
-filenames <- list.files(pattern="*_rformat_dist_0.03.txt")
+filenames <- list.files(pattern="*_rformat_dist_0.1.txt")
 
 #move back up directories
 setwd("../..")
 
 #make dataframes of all OTU tables
 for(i in filenames){
-  filepath <- file.path(paste(wd, "/data/0.03_clust", sep = ""),paste(i,sep=""))
-  assign(gsub("_rformat_dist_0.03.txt", "", i), read.delim(filepath,sep = "\t"))
+  filepath <- file.path(paste(wd, "/data/0.1_clust", sep = ""),paste(i,sep=""))
+  assign(gsub("_rformat_dist_0.1.txt", "", i), read.delim(filepath,sep = "\t"))
 }
 
 #change OTU to gene name
 colnames(acr3) <- gsub("OTU", deparse(substitute(acr3)), colnames(acr3))
-colnames(arsB) <- gsub("OTU", deparse(substitute(arsB)), colnames(arsB))
 colnames(aioA) <- gsub("OTU", deparse(substitute(aioA)), colnames(aioA))
 colnames(arsC_glut) <- gsub("OTU", deparse(substitute(arsC_glut)), colnames(arsC_glut))
 colnames(arsC_thio) <- gsub("OTU", deparse(substitute(arsC_thio)), colnames(arsC_thio))
@@ -43,11 +45,11 @@ colnames(intI) <- gsub("OTU", deparse(substitute(intI)), colnames(intI))
 colnames(vanA) <- gsub("OTU", deparse(substitute(vanA)), colnames(vanA))
 colnames(vanB) <- gsub("OTU", deparse(substitute(vanB)), colnames(vanB))
 colnames(vanH) <- gsub("OTU", deparse(substitute(vanH)), colnames(vanH))
+colnames(rplB) <- gsub("OTU", deparse(substitute(rplB)), colnames(rplB))
 
 #join together all files
 otu_table <- acr3 %>%
   left_join(aioA, by = "X") %>%
-  left_join(arsB, by = "X") %>%
   left_join(ClassA, by = "X") %>%
   left_join(ClassB, by = "X") %>%
   left_join(ClassC, by = "X") %>%
@@ -57,7 +59,16 @@ otu_table <- acr3 %>%
   left_join(vanH, by = "X") %>%
   left_join(arsC_glut, by = "X") %>%
   left_join(arsC_thio, by = "X") %>%
-  left_join(arsM, by = "X") 
+  left_join(rplB, by = "X") %>%
+  rename(Site = X)
+
+#count otu columns
+otus <- ncol(otu_table)
+
+#add in metadata
+otu_table <- otu_table %>%
+  left_join(meta, by = "Site") %>%
+  select(Site:As_ppm, SoilTemperature_to10cm, OrganicMatter_500:Fe_ppm)
 
 #add row names back
 rownames(otu_table)=otu_table[,1]
@@ -73,14 +84,14 @@ otu_table[is.na(otu_table)] <- 0
 
 otu_table.t <- t(otu_table)
 otu_table_norm <- otu_table.t
-n <- ncol(otu_table_norm)
-for(i in 1:n){otu_table_norm[,i]=otu_table.t[,i]/sum(otu_table.t[,i])}
+
+for(i in 1:12){otu_table_norm[,i]=otu_table.t[,i]/sum(otu_table.t[,i])}
 
 #make presence absence matrix
 otu_table_normPA <- (otu_table_norm>0)*1
 
 #list OTUs present in less than half of samples
-abund <- otu_table_normPA[which(rowSums(otu_table_normPA) > 3),]
+abund <- otu_table_normPA[which(rowSums(otu_table_normPA) > 5),]
 
 #remove OTUs with presence in less than 4 sites
 otu_table_norm.slim <- otu_table_norm[which(rownames(otu_table_norm) %in%
@@ -90,7 +101,7 @@ otu_table_norm.slim <- otu_table_norm[which(rownames(otu_table_norm) %in%
 otu_table_norm.slim.t <- t(otu_table_norm.slim)
 
 #find correlations between OTUs
-corr <- corr.test(otu_table_norm.slim.t, method = "spearman", adjust = "fdr")
+corr <- corr.test(otu_table_norm.slim.t, method = "spearman")
 
 #make network of correlations
-qgraph(corr$r, minimum = "sig", sampleSize=12, layout = "spring", details = TRUE, graph = "cor")
+qgraph(corr$r, minimum = "sig", sampleSize=12, layout = "spring", details = TRUE, graph = "cor", label.cex = 2, alpha = 0.01)
