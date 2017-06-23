d8e6a26aeb7c4ea955dbafeacc2ca9b7eadde92c
dunivint <dunivint@Taylors-MacBook-Pro-5.local>
2017-06-23 11:42:51 -0400

    nonpareil

---
+++ coverage/nonpareil.R
@@ -0,0 +1,3 @@
+source('/Users/dunivint/Documents/GitHubRepos/nonpareil/utils/Nonpareil.R')
+
+Nonpareil.curve('/Users/dunivint/Documents/GitHubRepos/Xander_arsenic/coverage/coverage.Cen03.txt.npo')
