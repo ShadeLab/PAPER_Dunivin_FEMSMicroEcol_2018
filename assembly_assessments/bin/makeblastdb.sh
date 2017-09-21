9f7ba7a210897739195672f211b9c58a8ef0ac08
dunivint <dunivint@msu.edu>
2017-09-21 13:44:46 -0400

    Update makeblastdb.sh

--- assembly_assessments/bin/makeblastdb.sh
+++ assembly_assessments/bin/makeblastdb.sh
@@ -3,15 +3,16 @@
 #you have to type the gene and sample names after calling this script, for example "$./testAutomation.sh arsB cen10"
 #$1 means the first argument (arsB in the example, so the variable GENE would be arsB), $2 is site name
 GENE=$1
+BASEDIR=$2
 
 #load blast
 module load GNU/4.4.5
 module load Gblastn/2.28
 
 #switch into correct directory. 
-cd /mnt/research/ShadeLab/WorkingSpace/Dunivin/xander/assessments/blast_databases
+cd ${BASEDIR}/blast_databases
 mkdir ${GENE}
 cd ${GENE}
 
 #make database from diverse gene sequences
-makeblastdb -in /mnt/research/ShadeLab/WorkingSpace/Dunivin/xander/analysis/RDPTools/Xander_assembler/gene_resource/${GENE}/originaldata/nucl.fa  -dbtype nucl -out ${GENE}_database
+makeblastdb -in ${BASEDIR}/RDPTools/Xander_assembler/gene_resource/${GENE}/originaldata/nucl.fa  -dbtype nucl -out ${GENE}_database
